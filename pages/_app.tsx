b7e34cbb7eb3d3e440c382bc1ac33120ce0fde63
YonnaR <yoann.forthulman@gmail.com>
2022-04-28 00:26:48 +0200

    add meta tag and description, update favicon, add manifest.json

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,9 @@ import '../assets/scss/style.scss';
 import '../assets/scss/custom.scss';
 import 'font-awesome/css/font-awesome.css';
 
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
-import Head from 'next/head';
 
 import { store } from '../src/store';
 
@@ -14,19 +14,21 @@ import AppToastFactory from '../src/components/Common/AppToastFactory';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
+    <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <AppHeader />
-      <div className="container-fluid">
-        <main>
-          <Component {...pageProps} />
-        </main>
-      </div>
-      <AppFooter />
-      <AppToastFactory />
-    </Provider>
+      <Provider store={store}>
+        <AppHeader />
+        <div className="container-fluid">
+          <main>
+            <Component {...pageProps} />
+          </main>
+        </div>
+        <AppFooter />
+        <AppToastFactory />
+      </Provider>
+    </>
   );
 }
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,29 @@
 import {
-  Html, Head, Main, NextScript,
+  Html,
+  Head,
+  Main,
+  NextScript,
 } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="fr">
       <Head>
         <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+        <meta name="Copyright" content="www.refugehulman.fr" />
+        <meta name="Designer" content="techyoon, www.techyoon.tk" />
+        <meta name="Publisher" content="techyoon, www.techyoon.tk" />
+
+        <meta name="keywords" content="marie galante, 'marie-galante', 'location marie galante', 'hotel marie galante', 'voiture marie galante', 'gite marie galante', 'location chambre marie galante',  'restaurant marie galante','bungalow marie galante', vacances, séjour, gites traditionnels, antilles, guadeloupe, caraïbe, location, piscine, 'culture antillaise', Campagne, Traditionnels,hotel, hébergement, bungalow, gites, photo, 'Refuge Hulman', 'Le refuge'" />
+        <meta name="description" content="Dans un cadre magnifique, entre cocotiers, flamboyants et arbres fruitiers entre miroitement de la mer et plumets argentés de cannes à sucre, Georges et Fortuna vous accueillent au REFUGE cuisine locale dont vous ferez l'éloge et hébergement tout confort qui vous feront passez les plus douces nuit dans l'un des plus beau cadre au monde : Saint-Louis de Marie-Galante en Guadeloupe" />
+
         <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="manifest.json" />
+        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+
       </Head>
       <body>
         <Main />
