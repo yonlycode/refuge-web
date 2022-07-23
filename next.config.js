329239ef09db1728e582f8328057e01a109bbaa4
Yonna <33960507+YonnaR@users.noreply.github.com>
2022-07-23 02:11:25 +0200

    feature - add path alias (#7)

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const { resolve } = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -7,6 +9,11 @@ const nextConfig = {
       test: /\.(jpe?g|png|svg|gif|ico|eot|ttf|woff|woff2|mp4|pdf|webm|txt)$/,
       type: 'asset/resource',
     });
+
+    config.resolve.alias['@'] = resolve(
+      __dirname,
+      'src',
+    );
 
     return config;
   },
